f0f653e27ba5f4265bc4b5c9ce56ed2b88d45e61
maviek14 <mt82032@stud.uph.edu.pl>
2024-02-07 16:36:09 +0100

    End of section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css'
 import { StoreContext, store } from './app/stores/store'
 import { RouterProvider } from 'react-router-dom'
@@ -9,7 +10,7 @@ import { router } from './app/router/Routes'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreContext.Provider value={store}>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </StoreContext.Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
