272ee81ad5ad2970d3562b7d590997736b32ae05
maviek14 <mt82032@stud.uph.edu.pl>
2024-02-21 13:07:18 +0100

    End of section 18

---
+++ Application/Profiles/Edit.cs
@@ -0,0 +1,53 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles;
+
+public class Edit
+{
+    public class Command : IRequest<Result<Unit>>
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.DisplayName).NotEmpty();
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, Result<Unit>>
+    {
+        private readonly DataContext context;
+        private readonly IUserAccessor userAccessor;
+
+        public Handler(DataContext context, IUserAccessor userAccessor)
+        {
+            this.context = context;
+            this.userAccessor = userAccessor;
+        }
+
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == userAccessor.GetUsername());
+
+            user.Bio = request.Bio ?? user.Bio;
+            user.DisplayName = request.DisplayName ?? user.DisplayName;
+
+            context.Entry(user).State = EntityState.Modified;
+
+            var success = await context.SaveChangesAsync() > 0;
+
+            if (success) return Result<Unit>.Success(Unit.Value);
+
+            return Result<Unit>.Failure("Problem updating profile");
+        }
+    }
+}
